7e286ad4b1b0595db7cdbedd2f0a699ccff31308
JeeviTester <suje8693@gmail.com>
2022-07-20 20:37:50 +1000

    Matt Code

--- src/test/java/org/test/SampleClass.java
+++ src/test/java/org/test/SampleClass.java
@@ -3,6 +3,8 @@ package org.test;
 public class SampleClass {
 	
 public static void main(String[] args) {
-	
+	System.out.println("TEST A1");
+	System.out.println("TEST A1");
+	System.out.println("TEST A1");
 }
 }

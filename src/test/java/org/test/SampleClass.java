f8e7a24359f2c302696fcd75a8fa050d15e2fd8f
JeeviTester <suje8693@gmail.com>
2022-07-20 20:15:12 +1000

    Laura's Code

--- src/test/java/org/test/SampleClass.java
+++ src/test/java/org/test/SampleClass.java
@@ -1,8 +1,11 @@
 package org.test;
 
 public class SampleClass {
-	
-public static void main(String[] args) {
-	
-}
+
+	public static void main(String[] args) {
+
+		System.out.println("TEST A");
+		System.out.println("TEST B");
+		System.out.println("TEST C");
+	}
 }
